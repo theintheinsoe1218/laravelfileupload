03e4fafe22e61d1f23754628e3f0c0967163988b
theintheinsoe1218 <theintheinsoe1218@gmail.com>
2022-06-26 20:27:01 +0630

    updating AppServiceProvier file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('APP_ENV')!== 'local'){
+            URL::forceScheme('https');
+        }
     }
 }
